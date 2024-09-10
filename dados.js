a57deecc2ad8584e5ae0f84e2572a7c99e91c5b9
Yasmim <120032313+Min-ysz@users.noreply.github.com>
2024-09-10 20:27:06 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma ginasta brasileira que se destacou em competições nacionais e internacionais. Sua elegância e força a tornaram uma das principais atletas da ginástica artística brasileira.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+      tags: "judô judo judoca judoka" // Tags corrigidas e simplificadas
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com sua performance nas Olimpíadas de Tóquio 2020. Sua história de superação e talento a tornaram um ícone do esporte.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginástica ginastica ouro"
+    }
+  ];
